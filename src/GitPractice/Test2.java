79c294ab2f284c4284aa5795fdf24c00dd42c2e3
PRASHANT SHIVCHARAN CHARPE <pcharpe6@gmail.com>
2023-06-14 15:55:59 +0530

    Revert "no code"
    This reverts commit 0d79ee588414d0501d11d68444f92125c15096d3.

--- src/GitPractice/Test2.java
+++ src/GitPractice/Test2.java
@@ -1,3 +1,13 @@
 public class Test2 {
 
+//Cloning this repo
+	public static void main(String[] args) {
+
+	System.out.println("prashant");
+	System.out.println("prashant");
+	}
+	public static void main() {
+	System.out.println("prashant");
+	System.out.println("prashant");
+	}
 }
