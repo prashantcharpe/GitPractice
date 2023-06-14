1eceaeb2fe79a170fa1ea5016ab88016e34764c4
PRASHANT SHIVCHARAN CHARPE <pcharpe6@gmail.com>
2023-06-14 15:18:03 +0530

    VERSION 3

--- src/GitPractice/Test2.java
+++ src/GitPractice/Test2.java
@@ -8,16 +8,9 @@ public class Test2 {
 	System.out.println("prashant");
 	System.out.println("prashant");
 	}
-	
-	public static void main() {
-	System.out.println("prashant");
-	System.out.println("prashant");
-	}
 
 	public static void main() {
 	System.out.println("prashant");
 	System.out.println("prashant");
 	}
-
-
 }
