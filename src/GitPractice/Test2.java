b50a526e9bf1b8209743cdb37318620a70d84098
PRASHANT SHIVCHARAN CHARPE <pcharpe6@gmail.com>
2023-06-14 15:48:33 +0530

    Revert "conflict is resolved"
    This reverts commit 72bee1febfd3f1de42015c33a45d890125df3693.

--- src/GitPractice/Test2.java
+++ src/GitPractice/Test2.java
@@ -6,8 +6,13 @@ public class Test2 {
 	System.out.println("prashant");
 	System.out.println("prashant");
 	}
+
 	public static void main() {
 	System.out.println("prashant");
 	System.out.println("prashant");
 	}
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> branchX
