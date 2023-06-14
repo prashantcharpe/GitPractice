0d79ee588414d0501d11d68444f92125c15096d3
PRASHANT SHIVCHARAN CHARPE <pcharpe6@gmail.com>
2023-06-14 15:52:00 +0530

    no code

--- src/GitPractice/Test2.java
+++ src/GitPractice/Test2.java
@@ -1,13 +1,3 @@
 public class Test2 {
 
-//Cloning this repo
-	public static void main(String[] args) {
-
-	System.out.println("prashant");
-	System.out.println("prashant");
-	}
-	public static void main() {
-	System.out.println("prashant");
-	System.out.println("prashant");
-	}
 }
